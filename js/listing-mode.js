08ab78559187cdaea6070f43d30a80d3e43d1143
viniciuspalma <vini_palma@outlook.com>
2014-09-11 18:48:42 -0300

    config of change modes to view projects

--- js/listing-mode.js
+++ js/listing-mode.js
@@ -1,8 +1,39 @@
 (function($, window){
-  var Prices = function() {
-    this.element = $('[data-listing-mode]');
+  var ListingMode = function() {
+    this.element       = $('[data-listing-mode]');
+    this.listingChange = this.element.find('[data-listing-change]');
+    this.wrapperProjects = $('[data-projects-wrapper]');
 
+    var that = this;
+
+    this.init = function() {
+      this.wrapperProjects.addClass('image');
+      this.element.find('[data-listing-change="image"]').addClass('active');
+      this.addEventListener();
+    };
+
+    this.controlModes = function(valueChange) {
+      this.wrapperProjects.removeClass('list image');
+      this.wrapperProjects.addClass(valueChange);
+    };
+
+    this.changeClasses = function(target, valueChange) {
+      this.listingChange.each(function(index, el) {
+        $(el).removeClass('active');
+      });
+      $(target).addClass('active');
+    };
+
+    this.addEventListener = function() {
+      that.listingChange.on('click', function(e){
+        var valueChange = $(e.target).data('listing-change');
+        that.controlModes(valueChange);
+        that.changeClasses(e.target, valueChange);
+      });
+    };
+
+    this.init();
   };
 
-  window.App.site.Prices = new Prices;
+  new ListingMode;
 })(jQuery, this);
